516c004564a33e2da427d9e2c0d6045a94e03421
Luke Sheppard <sheppiegit@gmail.com>
2024-01-27 21:10:25 -0500

    updated syntax errors in code.

--- brute_force_key.py
+++ brute_force_key.py
@@ -7,8 +7,8 @@ from Crypto.Cipher import AES
 
 
 plaintext = bytearray.fromhex('255044462d312e350a25bff7a2fe0a33')
-ciphertext: bytearray.fromhex("406ed6d5eba12a69ea7b69bf1ee05b8f")
-iv = ("6f6d6567616c756c3132333435363738")
+ciphertext= bytearray.fromhex("406ed6d5eba12a69ea7b69bf1ee05b8f")
+iv = bytearray.fromhex("6f6d6567616c756c3132333435363738")
 
 with open('all_possible_keys.txt') as f:
     keys = f.readlines()
@@ -17,7 +17,7 @@ for k in keys:
     k = k.rstrip('\n')
     key = bytearray.fromhex(k)
     cipher = AES.new(key=key, mode=AES.MODE_CBC, iv=iv)
-    guess = cipher.encrypt(data)
+    guess = cipher.encrypt(plaintext)
     if guess == ciphertext:
         print("find the key:", k)
         exit(0)
